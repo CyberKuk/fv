2fb6d32041a55901b9015dce96f6e16f70dfce1b
cah4a <cah4o3@gmail.com>
2013-02-01 19:02:57 +0200

    ViewModels Collection ViewModel

---
+++ data/libs/fv/ViewModel/ViewModelCollection.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace fv\ViewModel;
+
+use fv\ViewModel\Exception\ViewModelException;
+
+class ViewModelCollection extends ViewModel implements \Iterator {
+
+    private $viewModels;
+
+    /**
+     * @param ViewModel[] $viewModels
+     */
+    function __construct( array $viewModels ){
+        $this->viewModels = $viewModels;
+        foreach( $this->viewModels as $viewModel ){
+            if( ! $viewModel instanceof ViewModel )
+                throw new ViewModelException("Array must be array of ViewModels");
+        }
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    public function current() {
+        return current($this->viewModels);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    public function next() {
+        next( $this->viewModels );
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key() {
+        return key($this->viewModels);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+    public function valid() {
+        return isset($this->viewModels[ $this->key() ]);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    public function rewind() {
+        reset($this->viewModels);
+    }
+
+
+}
