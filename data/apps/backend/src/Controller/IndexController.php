f67560d1c3abc6af746ef340e21954350a853700
cah4a <cah4o3@gmail.com>
2013-02-01 15:19:44 +0200

    classes to src dirs

---
+++ data/apps/backend/src/Controller/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Application\Backend\Controller;
+
+use Bundle\fv\MetroUIBundle\Controller\IndexController as ParentController;
+
+class IndexController extends ParentController {
+}
